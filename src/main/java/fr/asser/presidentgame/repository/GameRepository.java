390dddde874d46dc5298a3bfc1057b45a6835862
francoismonteil <francois.monteil@protonmail.com>
2024-09-01 18:15:32 +0200

    chore: Recfacto GameRepository

--- src/main/java/fr/asser/presidentgame/repository/GameRepository.java
+++ src/main/java/fr/asser/presidentgame/repository/GameRepository.java
@@ -1,5 +1,6 @@
 package fr.asser.presidentgame.repository;
 
+import fr.asser.presidentgame.exception.GameNotFoundException;
 import fr.asser.presidentgame.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,4 +14,8 @@ public interface GameRepository extends JpaRepository<Game, Long> {
 
     @Query("SELECT g FROM Game g LEFT JOIN FETCH g.players LEFT JOIN FETCH g.deck LEFT JOIN FETCH g.playedCards WHERE g.id = :id")
     Optional<Game> findByIdWithAssociations(@Param("id") Long id);
+
+    default Game getByIdOrThrow(Long id) {
+        return findByIdWithAssociations(id).orElseThrow(() -> new GameNotFoundException(id));
+    }
 }
